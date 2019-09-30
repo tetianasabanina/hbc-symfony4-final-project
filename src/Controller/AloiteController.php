fb2ccedf7fb76eda6ce31ce1e5c91edf5e4b013f
tatiana_sabanina <tatiana08st@gmail.com>
2019-09-30 20:44:40 +0300

    Form asenettu,  luotu lomake (uusi.html.twig)

---
+++ src/Controller/AloiteController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AloiteLaatikko;
+use App\Form\AloiteFormType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class  AloiteController extends AbstractController {
+    /**
+     * @Route("/aloite", name="aloite_lista")
+     */
+
+     public function index() {
+        // Hakee kaikki aloitteet tietokannasta
+        $aloitteet = $this->getDoctrine()->getRepository(AloiteLaatikko::class)->findAll();
+        // var_dump($aloitteet);
+
+
+        // Pyydetään näkymää näytämään kaikki linkit
+        return $this->render('index.html.twig', [
+            'aloitteet' => $aloitteet,
+        ]);
+            
+    }       
+
+    /**
+     * @Route("/aloite/uusi", name="aloite_uusi")
+     */
+
+    public function uusi(Request $request) {
+        $aloite = new AloiteLaatikko();
+
+    //Luodaan lomake 
+    $form = $this->createForm(
+        AloiteFormType::class,
+        $aloite, [
+            'action'    => $this->generateUrl('aloite_uusi'),
+            'attr'      => ['class' => 'form-signin']
+        ]
+    );
+
+    //Käsitellän lomakeetta tulleet tiedot ja talletetaan tietokantaan
+    $form->handleRequest($request);
+    if($form->isSubmitted()){
+        //Talletetaan lomaketiedot muuttujaan
+        $aloite = $form->getData();
+
+        //Telletetaan tietokantaan
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($aloite);
+        $entityManager->flush();
+
+        //Kutsutaan index-kontrolleria
+        return $this->redirectToRoute('aloite_lista');
+    }
+
+        //Pyydetään näkymää näytämään lomake
+        return $this->render('uusi.html.twig', [
+            'form1' => $form->createView()
+        ]);
+    }
+    
+
+    /**
+     * @Route("/aloite/nayta/{id}", name="aloite_nayta")
+     */
+
+    public function nayta($id) {
+        $aloite = $this->getDoctrine()->getRepository(AloiteLaatikko::class)->find($id);
+
+
+        return $this->render('nayta.html.twig', [
+            'aloite' => $aloite,
+        ]);
+    }
+    
+
+    /**
+     * @Route("/aloite/muokkaa/{id}", name="aloite_muokkaa")
+     */
+
+    public function muokkaa(Request $request, $id) {
+       
+        $linkki = $this->getDoctrine()->getRepository(AloiteLaatikko::class)->find($id);
+
+        return $this->render('muokkaa.html.twig', [
+            'form1' => $form->createView()
+        ]);
+    }
+    
+
+    /**
+     * @Route("/aloite/poista/{id}", name="aloite_poista")
+     */
+
+    public function poista(Request $request, $id) {
+        $linkki = $this->getDoctrine()->getRepository(AloiteLaatikko::class)->find($id);
+
+        return $this->render('poista.html.twig');
+        //return $this->redirectToRoute('aloite_lista');
+
+    }
+    
+
+}
+
+?>
---
+++ src/Form/AloiteFormType.php
@@ -0,0 +1,39 @@
+<?php
+
+    namespace App\Form;
+
+    use App\Entity\AloiteLaatikko;
+    use Symfony\Component\Form\AbstractType;
+    use Symfony\Component\Form\FormBuilderInterface;
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
+    use Symfony\Component\Form\Extension\Core\Type\DateType;
+    use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+    use Symfony\Component\OptionsResolver\OptionsResolver;
+
+    class AloiteFormType extends AbstractType{
+
+        public function buildForm(FormBuilderInterface $builder, array $options) {
+            $builder
+                ->add('Aihe', TextType::class, ['label' => 'Aihe'])
+                ->add('Etunimi', TextType::class, ['label'=> 'Etunimi'])
+                ->add('Sukunimi', TextType::class, ['label'=> 'Sukunimi'])
+                ->add('Kuvaus', TextType::class, ['label'=> 'Kuvaus'])
+                ->add('Ote', TextType::class, ['label'=> 'Ote'])
+                ->add('Kirjauspaiva', DateType::class, ['label'=> 'Kirjauspaiva'])
+                ->add('Spostiosoite', TextType::class, ['label'=> 'Sähköpostiosoite'])
+                ->add('Save', SubmitType::class, [
+                    'label'=> 'Talenna',
+                    'attr' => ['class'=> 'btn btn-info']]);
+
+
+        }
+
+        public function configureOptions(OptionsResolver $resolver) {
+            $resolver->setDefaults([
+                'data-class' => AloiteLaatikko::class,
+
+            ]);
+        }
+    }
+
+?>
